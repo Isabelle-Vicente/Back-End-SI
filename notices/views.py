ecfb28a7cdc1fd9835cc3d0476beebd88dea407b
Isabelle-Vicente <73740552+Isabelle-Vicente@users.noreply.github.com>
2024-09-13 10:08:35 -0300

    Update notices/views.py

--- notices/views.py
+++ notices/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from users.permissions import IsAdmin
 from .models import Notice
 from .serializers import NoticeSerializer
+from rest_framework.decorators import action
 
 class NoticeViewSet(viewsets.ModelViewSet):
     queryset = Notice.objects.all()
@@ -12,13 +13,11 @@ class NoticeViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        # Define o usuário criador e responsável no momento da criação do aviso
         serializer.save(id_user=self.request.user, responsible=self.request.user)
 
     def update(self, request, *args, **kwargs):
         notice = self.get_object()
 
-        # Apenas o criador ou admin podem editar enquanto não aprovado
         if not notice.is_approved and (request.user == notice.id_user or request.user.is_admin):
             serializer = NoticeSerializer(notice, data=request.data, partial=True)
             if serializer.is_valid():
@@ -26,7 +25,6 @@ class NoticeViewSet(viewsets.ModelViewSet):
                 return Response(serializer.data)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        # Se aprovado, só o administrador pode editar
         if notice.is_approved and not request.user.is_admin:
             return Response({'error': 'Apenas o administrador pode editar um aviso aprovado.'}, status=status.HTTP_403_FORBIDDEN)
 
@@ -39,12 +37,17 @@ class NoticeViewSet(viewsets.ModelViewSet):
             return Response({'error': 'Apenas administradores podem aprovar avisos.'}, status=status.HTTP_403_FORBIDDEN)
 
         notice.is_approved = True
-        notice.responsible = request.user  # O admin se torna o responsável
+        notice.responsible = request.user
         notice.save()
         return Response({'status': 'Aviso aprovado com sucesso!'})
 
     def user_notices(self, request):
-        # Filtra avisos pertencentes ao usuário autenticado
         notices = Notice.objects.filter(id_user=request.user)
+        serializer = NoticeSerializer(notices, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def approved_notices(self, request):
+        notices = Notice.objects.filter(is_approved=True)
         serializer = NoticeSerializer(notices, many=True)
         return Response(serializer.data)
