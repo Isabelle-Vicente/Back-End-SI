bc7093046143a242074addf7b7ed858e57d2048b
Isabelle-Vicente <isabelle.v.evi@gmail.com>
2024-09-22 12:09:20 -0300

    feat: Max-leght em todos campos

--- users/models.py
+++ users/models.py
@@ -29,7 +29,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(max_length=100, unique=True)
     full_name = models.CharField(max_length=100)
     role = models.CharField(
-        max_length=20,
+        max_length=10,
         choices=ROLE_CHOICES,
         default=EDITOR,
     )
--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,8 @@ from .views import RegisterView, LoginView, UserView, LogoutView, ApproveUserVie
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
-    path('user', UserView.as_view(), name='user_detail'),
     path('logout', LogoutView.as_view(), name='logout'),
+    path('user', UserView.as_view(), name='user_detail'),
     path('approve-user/<uuid:pk>/', ApproveUserView.as_view(), name='approve_user'),
     path('users', UserViewSet.as_view({'get': 'list'}), name='user_list'),
     path('users/<uuid:pk>/', UserViewSet.as_view({'get': 'retrieve', 'patch': 'update', "delete": "destroy"}), name='user_detail'),
