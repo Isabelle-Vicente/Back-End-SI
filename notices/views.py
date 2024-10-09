fa964524466dbba82c0618593d3f96d1ba472791
bruno-pequeno <bru.pequeno@gmail.com>
2024-10-09 18:32:11 -0300

    fix: Corrigindo alterações da atualização de imagem

--- notices/views.py
+++ notices/views.py
@@ -25,10 +25,12 @@ class NoticeViewSet(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
+
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
 
         image_file = request.FILES.get('image_file')
+
         self.perform_update(serializer, image_file=image_file)
         
         return Response(serializer.data)
